32a1f5dc084773716c129f0d88d8c523c982216f
Rohith2409 <rohithpudathu@gmail.com>
2025-03-06 15:01:27 +0530

    TaskManager Frontend

--- TaskManager Backend/TaskManager/TaskManager/src/main/java/com/task/dao/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.task.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import com.task.entity.Task;
-
-@Repository
-public interface TaskRepository extends JpaRepository<Task, Long> {
-}
